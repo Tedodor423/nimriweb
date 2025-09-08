b6afca255aae896d22b5532ea76ee57aa47ce6c7
Tedodor423 <vrtulnik1@gmail.com>
2025-09-08 19:43:01 +0200

    testing assets folder

--- .eleventy.js
+++ .eleventy.js
@@ -2,15 +2,17 @@ require('dotenv').config();
 
 module.exports = function(eleventyConfig) {
   
-  eleventyConfig.addPassthroughCopy({
-    //"src/assets": "assets",
-    //"src/assets": "theo/assets",
-    "src/assets": "arnost/assets"
-  });
-  // eleventyConfig.addPassthroughCopy({ "src/assets": "theo/assets" });
-  // eleventyConfig.addPassthroughCopy({ "src/assets": "arnost/assets" });
+  // eleventyConfig.addPassthroughCopy({ 
+  //   "src/**/*.{png,jpg,jpeg,gif,svg}": "/" 
+  // });
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ "src/theo/assets": "theo/assets" });
+  eleventyConfig.addPassthroughCopy({ "src/arnost/assets": "arnost/assets" });
 
   return {
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
     dir: {
       input: "src",
       output: process.env.ELEVENTY_OUTPUT || "/var/www/html"
