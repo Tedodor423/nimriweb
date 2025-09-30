6f68e74247247db7ea45afa9026a9d028e4eddd2
Tedodor423 <vrtulnik1@gmail.com>
2025-09-30 13:43:43 +0200

    fix fonts

--- scripts/testserver.sh
+++ scripts/testserver.sh
@@ -6,7 +6,7 @@ mkdir _site/css
 
 # Ensure all background jobs are killed on exit (Ctrl+C or normal exit)
 trap 'kill 0' EXIT
-find src/scss -maxdepth 1 -type f ! -name "base.scss" -exec sh -c 'filename=$(basename "$1" .scss); npx sass --watch "$1" "_site/css/${filename}.css"' _ {} \; &
+find src/scss -maxdepth 1 -type f ! -name "base.scss" ! -name "_*" -exec sh -c 'filename=$(basename "$1" .scss); npx sass --watch "$1" "_site/css/${filename}.css" &' _ {} \;
 
 npx sass --watch src/scss/base.scss src/_includes/css/base.css &
 
